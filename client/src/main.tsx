7abf7c3a1284d2b7d98132afc483cf2acbd77322
DavidInvest <vd@suncapus.com>
2025-07-05 01:11:54 -0500

    Create main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
+Add React entry point file
